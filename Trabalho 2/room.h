ddef2da008c841f31526ac5e370dc3a1c2355173
César <cesar98alexandre@gmail.com>
2018-05-13 01:39:52 +0100

    Adição do reservas4.txt

--- Trabalho 2/room.h
+++ Trabalho 2/room.h
@@ -14,9 +14,8 @@
 ////////////
 
 #define MAX_ROOM_SEATS 9999
-#define MAX_CLI_SEATS 99
+#define MAX_CLI_SEATS 20
 #define WIDTH_PID 5
-#define WIDTH_XXNN 5
 #define WIDTH_SEAT 4
 #define WIDTH 2
 #define FREE 1
